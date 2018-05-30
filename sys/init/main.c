cca89c21af1e67feb0fcf1fec96084eddc235dc4
Christopher W. Olsen <cwolsen@BrainChurts.com>
2018-05-29 21:13:30 -0400

    Working on new boot code

--- sys/init/main.c
+++ sys/init/main.c
@@ -30,6 +30,7 @@
 #include <sys/gdt.h>
 #include <sys/video.h>
 #include <sys/tss.h>
+#include <sys/bootinfo.h>
 #include <ubixos/exec.h>
 #include <ubixos/kpanic.h>
 #include <ubixos/systemtask.h>
@@ -44,6 +45,18 @@
 #define B_CONTROLLERSHIFT       20
 #define B_CONTROLLERMASK        0xf
 #define B_CONTROLLER(val)       (((val)>>B_CONTROLLERSHIFT) & B_CONTROLLERMASK)
+/*
+ * Constants for converting boot-style device number to type,
+ * adaptor (uba, mba, etc), unit number and partition number.
+ * Type (== major device number) is in the low byte
+ * for backward compatibility.  Except for that of the "magic
+ * number", each mask applies to the shifted value.
+ * Format:
+ *       (4)   (8)   (4)  (8)     (8)
+ *      --------------------------------
+ *      |MA | SLICE | UN| PART  | TYPE |
+ *      --------------------------------
+ */
 #define B_SLICESHIFT            20
 #define B_SLICEMASK             0xff
 #define B_SLICE(val)            (((val)>>B_SLICESHIFT) & B_SLICEMASK)
@@ -56,6 +69,7 @@
 #define B_TYPESHIFT             0
 #define B_TYPEMASK              0xff
 #define B_TYPE(val)             (((val) >> B_TYPESHIFT) & B_TYPEMASK)
+
 
 /*****************************************************************************************
  Desc: The Kernels Descriptor table:
@@ -98,6 +112,11 @@ struct {
 static char *argv_init[2] = { "init", NULL, }; // ARGV For Initial Proccess
 static char *envp_init[6] = { "HOME=/", "PWD=/", "PATH=/bin:/sbin:/usr/bin:/usr/sbin", "USER=root", "GROUP=admin", NULL, }; //ENVP For Initial Proccess
 
+struct bootinfo _bootinfo;
+char _kernelname[512];
+u_long _bootdev;
+u_long _boothowto;
+
 /**
  * \brief This is the entry point into the os where all of the kernels sub systems are started up.
  *
@@ -119,7 +138,7 @@ int kmain(uint32_t rootdev) {
   /* New Root Mount Point */
   //Old 2 new 10
   kprintf("[0x%X][0x%X:0x%X:0x%X:0x%X:0x%X:0x%X]\n", B_ADAPTOR(rootdev), B_CONTROLLER(rootdev), B_SLICE(rootdev), B_UNIT(rootdev), B_PARTITION(rootdev), B_TYPE(rootdev));
-  //if ( vfs_mount( 0x1, B_PARTITION(rootdev) + 2, 0x0, 0xAA, "sys", "rw" ) != 0x0 ) {
+  //if ( vfs_mount( B_UNIT(_bootdev), B_PARTITION(_bootdev), 0x0, 0xAA, "sys", "rw" ) != 0x0 ) {
   if (vfs_mount(0x1, 0x2, 0x0, 0xAA, "sys", "rw") != 0x0) {
     kprintf("Problem Mounting sys Mount Point\n");
   }
@@ -141,12 +160,19 @@ int kmain(uint32_t rootdev) {
   kprintf("MemoryMap:  [0x%X]\n", vmmMemoryMap);
   kprintf("Starting OS\n");
 
-  execThread(systemTask, 0x2000, 0x0);
 
-  execFile("sys:/bin/init", argv_init, envp_init, 0x0); /* OS Initializer    */
 
   //kprintf("SDE Thread Start! [0x%X]\n", &sdeThread);
   //execThread(&sdeThread, 0x2000,0x0);
+
+  kprintf("Kernel Name: [%s], Boot How To [0x%X], Boot Dev: [0x%X]\n", _kernelname, _boothowto, _bootdev);
+  kprintf("B_TYPE(0x%X), B_SLICE(0x%X), B_UNIT(0x%X), B_PARTITION(0x%X)\n", B_TYPE(_bootdev), B_SLICE(_bootdev), B_UNIT(_bootdev), B_PARTITION(_bootdev));
+  kprintf("_bootinfo.bi_version: 0x%X\n", _bootinfo.bi_version);
+  kprintf("_bootinfo.bi_size: 0x%X\n", _bootinfo.bi_size);
+  kprintf("_bootinfo.bi_bios_dev: 0x%X\n", _bootinfo.bi_bios_dev);
+
+  execThread(systemTask, 0x2000, 0x0);
+  execFile("sys:/bin/init", argv_init, envp_init, 0x0); /* OS Initializer    */
 
   irqEnable(0x0);
 
