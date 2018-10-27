2756d09292df67ad3f6722012534f9ea58647984
MrOlsen <cwolsen@BrainChurts.com>
2018-10-26 20:23:25 -0400

    DEBUG

--- sys/kernel/vfs_calls.c
+++ sys/kernel/vfs_calls.c
@@ -74,7 +74,6 @@ int sys_openat(struct thread *td, struct sys_openat_args *args) {
      return(error);
 
 
-
   nfp->fd = fopen(args->path,"r");
 
   if (nfp->fd == 0x0) {
@@ -82,12 +81,26 @@ int sys_openat(struct thread *td, struct sys_openat_args *args) {
 
     td->td_retval[0] = -1;
     error = -1;
+
+    kprintf("[sOA: 0x%X:%s:%i]", args->mode, args->path, td->td_retval[0]);
+
+    if ((args->mode & O_RDONLY) == O_RDONLY)
+      kprintf("O_RDONLY");
+
+    if ((args->mode & O_WRONLY) == O_WRONLY)
+      kprintf("O_WRONLY");
+
+    if ((args->mode & O_RDWR) == O_RDWR)
+      kprintf("O_RDWR");
+
+    if ((args->mode & O_ACCMODE) == O_ACCMODE)
+      kprintf("O_ACCMODE");
+
   }
   else {
     td->td_retval[0] = fd;
   }
 
-  kprintf("[sOA: 0x%X:%s:%i]", args->mode, args->path, td->td_retval[0]);
 
   return (error);
   }
