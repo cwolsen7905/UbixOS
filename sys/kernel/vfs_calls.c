bad5596fe201c378fad636fb070aa08df3fbcdc8
Christopher W. Olsen <cwolsen@BrainChurts.com>
2018-11-09 13:55:13 -0500

    Why is errno not updating

--- sys/kernel/vfs_calls.c
+++ sys/kernel/vfs_calls.c
@@ -297,6 +297,7 @@ int sys_readlink(struct thread *thr, struct sys_readlink_args *args) {
   kprintf("Call to readlink\n");
   kprintf("Path: %s\n", args->path);
   kprintf("Count: %i\n", args->count);
+  kprintf("EDX: 0x%X", thr->td_retval[1]);
 
   //Return ERROr
   thr->td_retval[0] = 2;
