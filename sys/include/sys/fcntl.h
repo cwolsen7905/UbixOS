72a2b1b936aa2aa589a4657c450ad6fc12ab10ce
Christopher W. Olsen <cwolsen@BrainChurts.com>
2018-11-16 20:03:06 -0500

    Sync

--- sys/include/sys/fcntl.h
+++ sys/include/sys/fcntl.h
@@ -31,12 +31,63 @@
 
 #define AT_FDCWD                -100
 
+/* command values */
+#define F_DUPFD         0               /* duplicate file descriptor */
+#define F_GETFD         1               /* get file descriptor flags */
+#define F_SETFD         2               /* set file descriptor flags */
+#define F_GETFL         3               /* get file status flags */
+#define F_SETFL         4               /* set file status flags */
+#define F_GETOWN        5               /* get SIGIO/SIGURG proc/pgrp */
+#define F_SETOWN        6               /* set SIGIO/SIGURG proc/pgrp */
+#define F_GETLK         7               /* get record locking information */
+#define F_SETLK         8               /* set record locking information */
+#define F_SETLKW        9               /* F_SETLK; wait if blocked */
+
+/* Flag Values */
+#define FREAD           0x0001
+#define FWRITE          0x0002
+#define O_NONBLOCK      0x0004          /* no delay */
+#define O_APPEND        0x0008          /* set append mode */
+#define O_SHLOCK        0x0010          /* open with shared file lock */
+#define O_EXLOCK        0x0020          /* open with exclusive file lock */
+#define O_ASYNC         0x0040          /* signal pgrp when data ready */
+#define O_FSYNC         0x0080          /* synchronous writes */
+#define O_SYNC          0x0080          /* POSIX synonym for O_FSYNC */
+#define O_NOFOLLOW      0x0100          /* don't follow symlinks */
+#define O_CREAT         0x0200          /* create if nonexistent */
+#define O_TRUNC         0x0400          /* truncate to zero length */
+#define O_EXCL          0x0800          /* error if already exists */
+#define O_DIRECT        0x00010000
+#define O_RDONLY        0x0000          /* open for reading only */
+#define O_WRONLY        0x0001          /* open for writing only */
+#define O_RDWR          0x0002          /* open for reading and writing */
+#define O_ACCMODE       0x0003          /* mask for above modes */
+
+#define FHASLOCK        0x4000          /* descriptor holds advisory lock */
+
+/* F MAPPERS */
+#define FAPPEND         O_APPEND        /* kernel/compat */
+#define FASYNC          O_ASYNC         /* kernel/compat */
+#define FFSYNC          O_FSYNC         /* kernel */
+#define FNONBLOCK       O_NONBLOCK      /* kernel */
+#define FNDELAY         O_NONBLOCK      /* compat */
+#define O_NDELAY        O_NONBLOCK      /* compat */
+#define FPOSIXSHM       O_NOFOLLOW
+
 #define O_DIRECTORY     0x00020000      /* Fail if not directory */
 #define O_EXEC          0x00040000      /* Open for execute only */
 
-#define FEXEC           O_EXEC
+#define FCNTLFLAGS      (FAPPEND|FASYNC|FFSYNC|FNONBLOCK|FPOSIXSHM|O_DIRECT)
 
-/* bits to save after open */
-#define FMASK   (FREAD|FWRITE|FAPPEND|FASYNC|FFSYNC|FNONBLOCK|O_DIRECT|FEXEC)
+/*
+ #define FFLAGS(oflags)  ((oflags) + 1)
+ #define OFLAGS(fflags)  ((fflags) - 1)
+ */
+
+/* convert from open() flags to/from fflags; convert O_RD/WR to FREAD/FWRITE */
+#define FFLAGS(oflags)  ((oflags) & O_EXEC ? (oflags) : (oflags) + 1)
+#define OFLAGS(fflags)  ((fflags) & O_EXEC ? (fflags) : (fflags) - 1)
+
+#define FMASK (FREAD|FWRITE|FAPPEND|FASYNC|FFSYNC|FNONBLOCK|O_DIRECT|FEXEC)
 
 #endif /* !_SYS_FCNTL_H_ */
