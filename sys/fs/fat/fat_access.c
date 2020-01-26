ae24ef255c0605f2b7764c8546900f27e23c28bb
Christopher W. Olsen <cwolsen@BrainChurts.com>
2020-01-25 21:47:06 -0500

    Sync

--- sys/fs/fat/fat_access.c
+++ sys/fs/fat/fat_access.c
@@ -424,6 +424,7 @@ uint32 fatfs_get_file_entry(struct fatfs *fs, uint32 Cluster, char *name_to_find
 
           // Compare names to see if they match
           if (fatfs_compare_names(long_filename, name_to_find)) {
+            kprintf("DB[%s:%i]", __FILE__, __LINE__);
             memcpy(sfEntry, directoryEntry, sizeof(struct fat_dir_entry));
             return 1;
           }
