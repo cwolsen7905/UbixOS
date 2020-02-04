55a13304729a1790b88d3289ade84dfbc9b895bc
Christopher W. Olsen <cwolsen@BrainChurts.com>
2020-02-04 15:51:11 -0500

    Buffer

--- sys/kernel/vfs_calls.c
+++ sys/kernel/vfs_calls.c
@@ -311,14 +311,17 @@ int sys_write(struct thread *td, struct sys_write_args *uap) {
     td->td_retval[0] = uap->nbyte;
   }
   else if (uap->fd == 1 && ((struct file*) td->o_files[1])->fd == 0x0) {
-        // buffer = kmalloc(1024);
+        buffer = kmalloc(1024);
+        memset(buffer, 0x0, 1024);
         //kprintf("nbyte: %i", uap->nbyte);
-        //memcpy(buffer, uap->buf, uap->nbyte);
+        memcpy(buffer, uap->buf, uap->nbyte);
+        kprintf(buffer);
+        kfree(buffer);
         //kprintf(buffer);
         // kprintf(uap->buf);
         //kfree(buffer);
         //kprintf("[%s:%i]", __FILE__, __LINE__);
-        kprint(uap->buf);
+        //kprint(uap->buf);
     td->td_retval[0] = uap->nbyte;
   }
   else {
